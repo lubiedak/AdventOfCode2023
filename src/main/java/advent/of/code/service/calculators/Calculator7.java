de35274ddb3da6ffe413c1ddd5df33e639414ee9
lubiedak <l.biedak@wp.pl>
2024-01-15 10:03:13 +0100

    Task7

--- src/main/java/advent/of/code/service/calculators/Calculator7.java
+++ src/main/java/advent/of/code/service/calculators/Calculator7.java
@@ -1,5 +1,6 @@
 package advent.of.code.service.calculators;
 
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
@@ -9,22 +10,42 @@ import java.util.stream.IntStream;
 @Component
 public class Calculator7 implements TaskCalculator {
 
-    private final static String CARDS = "AKQJT98765432";
+    private final static String CARDS = "AKQT98765432J";
     private final static ArrayList<String> HAND_TYPE = new ArrayList<>(Arrays.asList("abcde", "aabcd", "aabbc", "aaabc", "aaabb", "aaaab", "aaaaa"));
 
     @Override
     public String calculate1(List<String> lines) {
-        var sorted = sortCards("AQ223");
-        int rank = handStrength(sorted);
-
-        return
-
-                "Not implemented";
+        var cards = new ArrayList<Hand>();
+        for (String line : lines) {
+            var hand = line.split("\\s+")[0];
+            int bet = Integer.parseInt(line.split("\\s+")[1]);
+            var sorted = sortCards(hand);
+            var maped = mapHand(hand);
+            cards.add(new Hand(hand, sorted, maped, handStrength(sorted), bet));
+        }
+        return "" + sumBets(cards);
     }
 
     @Override
     public String calculate2(List<String> lines) {
-        return "Not implemented";
+        var cards = new ArrayList<Hand>();
+        for (String line : lines) {
+            var hand = line.split("\\s+")[0];
+            int bet = Integer.parseInt(line.split("\\s+")[1]);
+            var sorted = sortCards2(hand);
+            var maped = mapHand2(hand);
+            cards.add(new Hand(hand, sorted, maped, handStrength(sorted), bet));
+        }
+        return "" + sumBets(cards);
+    }
+
+    private long sumBets(ArrayList<Hand> cards) {
+        cards.sort(Comparator.comparingInt(Hand::rank).thenComparing(Hand::cardsMaped, Comparator.naturalOrder()));
+        long sum = 0;
+        for (int i = 0; i < cards.size(); i++) {
+            sum += cards.get(i).bet() * (i + 1);
+        }
+        return sum;
     }
 
     private int handStrength(String hand) {
@@ -42,10 +63,23 @@ public class Calculator7 implements TaskCalculator {
         return HAND_TYPE.indexOf(handMapped);
     }
 
+    private String mapHand(String hand) {
+        hand = hand.replace('A', 'Z');
+        hand = hand.replace('K', 'Y');
+        hand = hand.replace('Q', 'X');
+        hand = hand.replace('J', 'W');
+        return hand;
+    }
+
+    private String mapHand2(String hand) {
+        hand = mapHand(hand);
+        hand = hand.replace('W', '1');
+        return hand;
+    }
+
     private String sortCards(String hand) {
-        Character[] handArray =
-                hand.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
-        Map<Character, Integer> map = new TreeMap<>(Comparator.comparingInt(CARDS::indexOf));
+        Character[] handArray = hand.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
+        Map<Character, Integer> map = new TreeMap<>();
 
         for (var card : handArray) {
             if (map.containsKey(card)) {
@@ -60,7 +94,40 @@ public class Calculator7 implements TaskCalculator {
                 .map(e -> IntStream.range(0, e.getValue()).mapToObj(index -> "" + e.getKey()).collect(Collectors.joining()))
                 .map(String::valueOf)
                 .collect(Collectors.joining());
+    }
 
+    private String sortCards2(String hand) {
+        Character[] handArray = hand.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
+        Map<Character, Integer> map = new TreeMap<>();
+        var maxChar = Arrays.stream(handArray).min(Comparator.comparingInt(CARDS::indexOf));
+        Character maxValue = 'J';
+        int maxVal = 1;
+        for (var card : handArray) {
+            if (map.containsKey(card)) {
+                map.put(card, map.get(card) + 1);
+                if (map.get(card) > maxVal && card != 'J') {
+                    maxVal = map.get(card);
+                    maxValue = card;
+                }
+            } else {
+                map.put(card, 1);
+            }
+        }
+
+        if (map.containsKey('J') && map.size() > 1) {
+            if (maxValue != 'J') {
+                map.put(maxValue, map.get(maxValue) + map.get('J'));
+            } else {
+                map.put(maxChar.get(), map.get(maxChar.get()) + map.get('J'));
+
+            }
+            map.remove('J');
+        }
+
+        return map.entrySet().stream().sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
+                .map(e -> IntStream.range(0, e.getValue())
+                        .mapToObj(index -> "" + e.getKey())
+                        .collect(Collectors.joining())).map(String::valueOf).collect(Collectors.joining());
     }
 
     @Override
@@ -68,5 +135,7 @@ public class Calculator7 implements TaskCalculator {
         return 7;
     }
 
+    record Hand(String cards, String sorted, String cardsMaped, int rank, int bet) {
+    }
 }
 
---
+++ src/main/java/advent/of/code/service/calculators/Calculator8.java
@@ -0,0 +1,23 @@
+package advent.of.code.service.calculators;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Calculator8 implements TaskCalculator {
+    @Override
+    public String calculate1(List<String> lines) {
+        return "Not implemented";
+    }
+
+    @Override
+    public String calculate2(List<String> lines) {
+        return "Not implemented";
+    }
+
+    @Override
+    public int getId() {
+        return 8;
+    }
+}
