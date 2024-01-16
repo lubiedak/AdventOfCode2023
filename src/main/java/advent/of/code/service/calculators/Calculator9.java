15c62e5cabc81ad9f40f26a3d2d4114baee35305
lubiedak <l.biedak@wp.pl>
2024-01-16 15:24:22 +0100

    Day9 part2

--- src/main/java/advent/of/code/service/calculators/Calculator9.java
+++ src/main/java/advent/of/code/service/calculators/Calculator9.java
@@ -27,7 +27,6 @@ public class Calculator9 implements TaskCalculator {
             sum += predictLast(sequences, seq);
         }
 
-
         return "\n" + sum;
     }
 
@@ -48,9 +47,35 @@ public class Calculator9 implements TaskCalculator {
         return predicted;
     }
 
+    private long predictFirst(Long[][] sequences, int last) {
+        long predicted = sequences[last-1][0];
+        for (int i = last - 2; i >= 0; i--) {
+            predicted =  sequences[i][0] - predicted;
+        }
+        return predicted;
+    }
+
     @Override
     public String calculate2(List<String> lines) {
-        return "Not implemented";
+        long sum = 0;
+        for (var line : lines) {
+            Long[][] sequences = new Long[100][];
+            Long[] sequence = Arrays.stream(line.split(" ")).map(Long::parseLong).toArray(Long[]::new);
+
+            int seq = 0;
+            do {
+                sequences[seq] = sequence;
+                var newSequence = new Long[sequence.length - 1];
+                for (int i = 1; i < sequence.length; i++) {
+                    newSequence[i - 1] = sequence[i] - sequence[i - 1];
+                }
+                sequence = newSequence;
+                seq++;
+            } while (!allAreZeros(sequence));
+            sum += predictFirst(sequences, seq);
+        }
+
+        return "\n" + sum;
     }
 
     @Override
