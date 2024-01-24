4fb5e9bcde8777a27518f80acaf96d933b3b64ff
lubiedak <l.biedak@wp.pl>
2024-01-24 07:54:12 +0100

    Day14 part 2

--- src/main/java/advent/of/code/service/calculators/Calculator14.java
+++ src/main/java/advent/of/code/service/calculators/Calculator14.java
@@ -2,7 +2,9 @@ package advent.of.code.service.calculators;
 
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class Calculator14 implements TaskCalculator {
@@ -13,16 +15,48 @@ public class Calculator14 implements TaskCalculator {
     @Override
     public String calculate1(List<String> lines) {
         var schema = readSchema(lines);
-        long then = System.currentTimeMillis();
         schema = rollNorth(schema);
 
-        System.out.println(System.currentTimeMillis() - then);
+        return "" + sumLoad(schema);
+    }
+
+    @Override
+    public String calculate2(List<String> lines) {
+        var schema = readSchema(lines);
+
+        /*
+        We assume that after couple hundreds roll we go into some cycle. So we do many rolls, and then start to discover a sequence.
+        For bigger problems it may fail. Also it may fail if 2 consecutive sumLoads are exactly the same.
+         */
+        int SKIPPED = 1500;
+        String seqence = "";
+        for (int i = 0; i < 5000; i++) {
+            schema = rollNorth(schema);
+            schema = rollWest(schema);
+            schema = rollSouth(schema);
+            schema = rollEast(schema);
+            if (i >= SKIPPED) {
+                int sum = sumLoad(schema);
+                seqence += sum + " ";
+                var mid = seqence.length() / 2;
+                if (seqence.substring(0, mid).equals(seqence.substring(mid))) {
+                    break;
+                }
+            }
+        }
+
+        var realSequence = seqence.substring(seqence.length() / 2).split(" ");
+        int index = (1000000000 - SKIPPED - 1) % realSequence.length;
+        return realSequence[index];
+    }
+
+    private int sumLoad(char[][] schema) {
         int i = 1;
-        long sum = 0;
+        int sum = 0;
         for (int row = schema.length - 1; row >= 0; row--) {
             sum += (i++) * (new String(schema[row]).chars().filter(c -> c == LOAD).count());
         }
-        return "" + sum;
+        return sum;
     }
 
     private char[][] rollNorth(char[][] schema) {
@@ -30,8 +64,8 @@ public class Calculator14 implements TaskCalculator {
             for (int row = 0; row < schema.length; row++) {
                 if (schema[row][col] == LOAD) {
                     int currentRow = row;
-                    while (canGoUp(schema, currentRow, col)) {
-                        schema = swapNorth(schema, currentRow--, col);
+                    while (canGo(schema, currentRow - 1, col)) {
+                        schema = swap(schema, currentRow, col, --currentRow, col);
                     }
                 }
             }
@@ -39,20 +73,55 @@ public class Calculator14 implements TaskCalculator {
         return schema;
     }
 
-    private boolean canGoUp(char[][] schema, int row, int col) {
-        int oneUp = row - 1;
-        return oneUp >= 0 && schema[oneUp][col] == SPACE;
+    private char[][] rollSouth(char[][] schema) {
+        for (int col = 0; col < schema[0].length; col++) {
+            for (int row = schema.length - 1; row >= 0; row--) {
+                if (schema[row][col] == LOAD) {
+                    int currentRow = row;
+                    while (canGo(schema, currentRow + 1, col)) {
+                        schema = swap(schema, currentRow, col, ++currentRow, col);
+                    }
+                }
+            }
+        }
+        return schema;
     }
 
-
-    @Override
-    public String calculate2(List<String> lines) {
-        return "Not implemented";
+    private char[][] rollEast(char[][] schema) {
+        for (int row = 0; row < schema.length; row++) {
+            for (int col = schema[0].length - 1; col >= 0; col--) {
+                if (schema[row][col] == LOAD) {
+                    int currentCol = col;
+                    while (canGo(schema, row, currentCol + 1)) {
+                        schema = swap(schema, row, currentCol, row, ++currentCol);
+                    }
+                }
+            }
+        }
+        return schema;
     }
 
-    private char[][] swapNorth(char[][] schema, int h, int w) {
-        char temp = schema[h - 1][w];
-        schema[h - 1][w] = schema[h][w];
+    private char[][] rollWest(char[][] schema) {
+        for (int row = 0; row < schema.length; row++) {
+            for (int col = 0; col < schema[0].length; col++) {
+                if (schema[row][col] == LOAD) {
+                    int currentCol = col;
+                    while (canGo(schema, row, currentCol - 1)) {
+                        schema = swap(schema, row, currentCol, row, --currentCol);
+                    }
+                }
+            }
+        }
+        return schema;
+    }
+
+    private boolean canGo(char[][] schema, int row, int col) {
+        return row >= 0 && row < schema.length && col >= 0 && col < schema[0].length && schema[row][col] == SPACE;
+    }
+
+    private char[][] swap(char[][] schema, int h, int w, int h1, int w1) {
+        char temp = schema[h1][w1];
+        schema[h1][w1] = schema[h][w];
         schema[h][w] = temp;
         return schema;
     }
