f4eb671dc05e907bbf4310a015f6a592bc32eb30
lubiedak <l.biedak@wp.pl>
2024-01-25 09:03:35 +0100

    Task13 cleaning and part2

--- src/main/java/advent/of/code/service/calculators/Calculator13.java
+++ src/main/java/advent/of/code/service/calculators/Calculator13.java
@@ -9,21 +9,33 @@ import java.util.List;
 public class Calculator13 implements TaskCalculator {
     @Override
     public String calculate1(List<String> lines) {
+        return "" + sumRefflectionsWithDiff(lines, 0);
+    }
+
+
+    @Override
+    public String calculate2(List<String> lines) {
+        return "" + sumRefflectionsWithDiff(lines, 1);
+    }
+
+    private int sumRefflectionsWithDiff(List<String> lines, int maxDiff) {
         List<String> currentBlock = new ArrayList<>();
         int sum = 0;
         for (var line : lines) {
             if (!line.isEmpty()) {
                 currentBlock.add(line);
             } else {
-                int rowSymmetry = findReflection(currentBlock);
-                currentBlock = transpose(currentBlock);
-                int columnSymmetry = findReflection(currentBlock);
+                int rowSymmetry = findReflection(currentBlock, maxDiff);
+                int columnSymmetry = 0;
+                if (rowSymmetry == 0) {
+                    currentBlock = transpose(currentBlock);
+                    columnSymmetry = findReflection(currentBlock, maxDiff);
+                }
                 sum += 100 * rowSymmetry + columnSymmetry;
                 currentBlock = new ArrayList<>();
             }
         }
-
-        return "" + sum;
+        return sum;
     }
 
     private List<String> transpose(List<String> currentBlock) {
@@ -38,27 +50,31 @@ public class Calculator13 implements TaskCalculator {
         return transposedBlock;
     }
 
-    private int findReflection(List<String> currentBlock) {
+    private int findReflection(List<String> currentBlock, int maxDiff) {
         for (int i = 0; i < currentBlock.size() - 1; i++) {
-            if (currentBlock.get(i).equals(currentBlock.get(i + 1))) {
-                var equals = true;
-                var begin = i - 1;
-                var end = i + 2;
-                while (begin >= 0 && end < currentBlock.size() && equals) {
-                    equals = currentBlock.get(begin--).equals(currentBlock.get(end++));
-                }
-                if (equals) {
-                    return i + 1;
-                }
+            int diff = 0;
+            var begin = i;
+            var end = i + 1;
+            while (begin >= 0 && end < currentBlock.size() && diff <= maxDiff) {
+                diff += differsBy(currentBlock.get(begin--), currentBlock.get(end++));
+            }
+            if (diff == maxDiff) {
+                return i + 1;
             }
         }
         return 0;
     }
 
-    @Override
-    public String calculate2(List<String> lines) {
-        return "Not implemented";
+    private int differsBy(String s1, String s2) {
+        var chars1 = s1.toCharArray();
+        var chars2 = s2.toCharArray();
+        int diffSum = 0;
+        for (int i = 0; i < chars1.length; i++) {
+            diffSum += chars1[i] == chars2[i] ? 0 : 1;
+        }
+        return diffSum;
     }
+
 
     @Override
     public int getId() {
