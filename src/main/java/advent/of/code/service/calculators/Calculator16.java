3167c508079a3218788c1d43ac3108b1935d1867
lubiedak <l.biedak@wp.pl>
2024-01-29 08:13:03 +0100

    Day16 part1

--- src/main/java/advent/of/code/service/calculators/Calculator16.java
+++ src/main/java/advent/of/code/service/calculators/Calculator16.java
@@ -4,35 +4,21 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Map;
+import java.util.function.Consumer;
 
+import static advent.of.code.service.calculators.Calculator16.D.*;
 import static advent.of.code.service.calculators.CalculatorUtils.readSchema;
 
 @Component
 public class Calculator16 implements TaskCalculator {
 
+    static Map<D, Integer> directions = Map.of(RIGHT, 1, LEFT, 2, UP, 4, DOWN, 8);
+
     @Override
     public String calculate1(List<String> lines) {
         var schema = readSchema(lines);
-        int height = schema.length;
-        int width = schema[0].length;
-
-        var schemaMarked = schema;
-
-        List<BeamStream> beams = new ArrayList<>();
-        beams.add(new BeamStream(new Position(0, 0), D.RIGHT));
-        schemaMarked[0][0] = '#';
-        while (!areAllFinished(beams, height, width)) {
-            beams.forEach(BeamStream::next);
-            beams.forEach(beam -> schemaMarked[beam.position.h][beam.position.w] = '#');
-            beams.forEach(beam -> {
-                    var symbol = schema[beam.position.h][beam.position.w];
-                    }
-            );
-        }
-
-
-        return "Not implemented";
+        return "" + calculate(schema, new BeamStream(new Position(0, 0), RIGHT));
     }
 
     @Override
@@ -40,16 +26,61 @@ public class Calculator16 implements TaskCalculator {
         return "Not implemented";
     }
 
+    private int calculate(char[][] schema, BeamStream startingBeam) {
+        int height = schema.length;
+        int width = schema[0].length;
 
-    boolean areAllFinished(List<BeamStream> beams, int height, int width) {
-        return beams.stream().allMatch(b -> b.isFinished(height, width));
+        int[][] schemaMarked = new int[height][width];
+        List<BeamStream> beams = new ArrayList<>();
+        beams.add(startingBeam);
+
+        while (!beams.isEmpty()) {
+            // We mark that given field was visited from this direction using '|=' operator
+            beams.forEach(beam -> schemaMarked[beam.position.row][beam.position.col] |= directions.get(beam.direction));
+            List<BeamStream> newBeams = new ArrayList<>();
+            beams.forEach(interactWithAMirror(schema, newBeams));
+            beams.addAll(newBeams);
+            beams.removeIf(b -> b.isFinished(height, width) || b.beenThere(schemaMarked) > 0);
+
+            beams.forEach(BeamStream::next);
+        }
+        return sumEnergizedCells(schemaMarked);
     }
 
-    class BeamStream {
+    private static int sumEnergizedCells(int[][] schemaMarked) {
+        int sum = 0;
+        for (int row = 0; row < schemaMarked.length; row++) {
+            for (int col = 0; col < schemaMarked[0].length; col++) {
+                if (schemaMarked[row][col] != 0) {
+                    sum++;
+                }
+            }
+        }
+        return sum;
+    }
+
+    private static Consumer<BeamStream> interactWithAMirror(char[][] schema, List<BeamStream> newBeams) {
+        return beam -> {
+            int row = beam.position.row;
+            int col = beam.position.col;
+            var symbol = schema[row][col];
+            if (symbol == '/' || symbol == '\\') {
+                beam.directionUpdate(symbol);
+            }
+            if (symbol == '|' && (beam.direction == RIGHT || beam.direction == LEFT)) {
+                beam.setDirection(UP);
+                newBeams.add(new BeamStream(new Position(row, col), DOWN));
+            }
+            if (symbol == '-' && (beam.direction == UP || beam.direction == DOWN)) {
+                beam.setDirection(RIGHT);
+                newBeams.add(new BeamStream(new Position(row, col), LEFT));
+            }
+        };
+    }
+
+    static class BeamStream {
         Position position;
         D direction;
-        Position prev;
-        D from;
 
         BeamStream(Position position, D direction) {
             this.position = position;
@@ -57,46 +88,65 @@ public class Calculator16 implements TaskCalculator {
         }
 
         void next() {
-            prev = new Position(position.h, position.w);
             position = position.next(direction);
-            from = position.cameFrom(prev);
+        }
+
+        void directionUpdate(char symbol) {
+            if (symbol == '/') {
+                direction = switch (direction) {
+                    case UP -> RIGHT;
+                    case DOWN -> LEFT;
+                    case LEFT -> DOWN;
+                    case RIGHT -> UP;
+                };
+            }
+            if (symbol == '\\') {
+                direction = switch (direction) {
+                    case UP -> LEFT;
+                    case DOWN -> RIGHT;
+                    case LEFT -> UP;
+                    case RIGHT -> DOWN;
+                };
+            }
+        }
+
+        void setDirection(D direction) {
+            this.direction = direction;
         }
 
         boolean isFinished(int height, int width) {
-            return direction == D.RIGHT && position.w >= width - 1
-                    || direction == D.LEFT && position.w <= 0
-                    || direction == D.UP && position.h <= 0
-                    || direction == D.DOWN && position.h >= height - 1;
+            return (direction == RIGHT && position.col >= width - 1)
+                    || (direction == LEFT && position.col <= 0)
+                    || (direction == UP && position.row <= 0)
+                    || (direction == DOWN && position.row >= height - 1);
+        }
+
+        int beenThere(int[][] schema) {
+            int h = position.row;
+            int w = position.col;
+            int nextMarker = switch (direction) {
+                case UP -> schema[h - 1][w];
+                case DOWN -> schema[h + 1][w];
+                case LEFT -> schema[h][w - 1];
+                case RIGHT -> schema[h][w + 1];
+            };
+            return nextMarker & directions.get(direction);
         }
     }
 
-    record Position(int h, int w) {
-        D cameFrom(Position previous) {
-            if (previous.h == h && previous.w - w == 1)
-                return D.RIGHT;
-            if (previous.h == h && previous.w - w == -1)
-                return D.LEFT;
-            if (previous.h - h == 1 && previous.w == w)
-                return D.DOWN;
-            if (previous.h - h == -1 && previous.w == w)
-                return D.UP;
-            return D.WRONG;
-        }
+    record Position(int row, int col) {
 
         Position next(D direction) {
             return switch (direction) {
-                case UP -> new Position(h - 1, w);
-                case DOWN -> new Position(h + 1, w);
-                case LEFT -> new Position(h, w - 1);
-                case RIGHT -> new Position(h, w + 1);
-                case WRONG -> null;
+                case UP -> new Position(row - 1, col);
+                case DOWN -> new Position(row + 1, col);
+                case LEFT -> new Position(row, col - 1);
+                case RIGHT -> new Position(row, col + 1);
             };
         }
     }
 
-    enum D {UP, DOWN, LEFT, RIGHT, WRONG}
-
-    ;
+    enum D {UP, DOWN, LEFT, RIGHT}
 
     @Override
     public int getId() {
